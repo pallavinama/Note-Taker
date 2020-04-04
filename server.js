1cb580f93ca284753b0939417131b9691db3ff45
pallavi <pallavi_nama@yahoo.com>
2020-04-03 22:10:16 -0400

    Adding final changes and pushing to heroku

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+// const port = 3000;
 
 app.use(express.urlencoded({
     extended: true
@@ -75,4 +76,5 @@ app.delete('/api/notes/:id', function(req,res){
     });
 });
 
-app.listen(port, () => console.log("Example app listening at http://localhost:"+port));
+app.listen(PORT, function(){
+});
