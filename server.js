d4add0b88622097567b41d5603fce5e494e6f916
pallavi <pallavi_nama@yahoo.com>
2020-04-03 21:24:32 -0400

    Add note code

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(express.static("public"));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname,'public/index.html'));
 });
@@ -16,6 +22,33 @@ app.get('/notes', function(req, res) {
 
 app.get('/api/notes', function(req, res) {
     res.sendFile(path.join(__dirname,'db/db.json'));
+});
+
+
+app.post('/api/notes', function(req,res) {
+    var newNote = req.body;
+    
+    fs.readFile(path.join(__dirname,"db/db.json"),'utf8',function(err,fileData){
+        if(err){
+            console.log("error reading file"+err);
+        }
+        const notes = JSON.parse(fileData);
+        
+        const id = notes.length+1;
+        newNote.id = id;
+        console.log("new note id "+newNote.id);
+        notes.push(newNote);
+        // reset the id's
+        for (var i = 0; i < notes.length; i++) {
+            notes[i].id = i+1;
+        }
+        res.json(newNote);
+        fs.writeFile(path.join(__dirname,"db/db.json"),JSON.stringify(notes,null,2), function(err){
+            if (err){
+                throw err;
+            }
+        });
+    });
 });
 
 
