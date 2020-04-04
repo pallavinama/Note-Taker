52f0563568a42bec0731ca7457d3d1f62d0f6880
pallavi <pallavi_nama@yahoo.com>
2020-04-03 21:35:18 -0400

    Delete notes

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/api/notes', function(req,res) {
         
         const id = notes.length+1;
         newNote.id = id;
-        console.log("new note id "+newNote.id);
+        
         notes.push(newNote);
         // reset the id's
         for (var i = 0; i < notes.length; i++) {
@@ -51,5 +51,28 @@ app.post('/api/notes', function(req,res) {
     });
 });
 
+app.delete('/api/notes/:id', function(req,res){
+    const id = req.params.id;
+    
+    fs.readFile(path.join(__dirname,"db/db.json"),'utf8',function(err,fileData){
+        if(err){
+            console.log("error reading file"+err);
+        }
+       
+        const notes = JSON.parse(fileData);
+        
+        notes.splice(id-1,1);
+        // reset the id's
+        for (var i = 0; i < notes.length; i++) {
+            notes[i].id = i+1;
+        }
+        res.json(notes);
+        fs.writeFile(path.join(__dirname,"db/db.json"),JSON.stringify(notes,null,2), function(err){
+            if (err){
+                throw err;
+            }
+        });
+    });
+});
 
 app.listen(port, () => console.log("Example app listening at http://localhost:"+port));
